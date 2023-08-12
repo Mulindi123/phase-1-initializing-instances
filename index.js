82652c5898ff24cb52f6db2ccd1a9a1e62bd6ed5
Medrine Musabi Mulindi <medrine.mulindi@student.moringaschool.com>
2023-08-12 22:29:30 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Write your code here
+class Breakfast{
+    constructor(food, drink){
+    this.food = food;
+    this.drink = drink;
+    }
+}
+
+class Lunch{
+    constructor(salad, soup, drink){
+    this.salad = salad;
+    this.drink= drink;
+    this.soup = soup;
+    }
+}
+
+class Dinner extends Lunch{
+    #dessert;
+    constructor(salad, soup, entree, dessert){
+        super(salad,soup);
+        this.entree = entree;
+        this.#dessert= dessert;
+        
+    }
+}
+
+let breakfast = new Breakfast("Nduma","coffee");
+console.log(breakfast);
+let lunch = new Lunch("fruit salad", "pumpkin soup","tea");
+console.log(lunch);
+let dinner = new Dinner("vegetables","lentiles","ice cream", "mousse");
+console.log(dinner)
